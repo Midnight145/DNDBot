14a5263dc0807f0047048a7494dd89cc0b430bb2
Ryan O'Dell <rubikscubeboy27@gmail.com>
2022-04-19 18:01:36 -0500

    standardize table names, no longer commit to db

--- modules/CampaignSQLHelper.py
+++ modules/CampaignSQLHelper.py
@@ -1,8 +1,9 @@
+import sqlite3
 from typing import Union, Optional, TYPE_CHECKING
 
 import discord
 
-from modules import CampaignInfo
+from .CampaignInfo import CampaignInfo
 
 if TYPE_CHECKING:  # TYPE_CHECKING is always false, allows for type hinting without circular import
     from ..bot import DNDBot
@@ -16,41 +17,45 @@ class CampaignSQLHelper:
     def __init__(self, bot: 'DNDBot'):
         self.bot = bot
 
-    def create_campaign(self, name: str, dm_id: int, vals: CampaignInfo, min_players: int, max_players: int) -> bool:
+    def create_campaign(self, vals: CampaignInfo) -> bool:
         """
         Adds a new campaign to the database
-        :param name: Campaign name
-        :param dm_id: Dungeon master member ID
         :param vals: Campaign info created by SQLHelper
-        :param min_players: Minimum players required
-        :param max_players: Maximum players allowed
-        :return: Whether campaign was created successfully
+        :param current_players: Current players in the campaign
+        :return: Whether we should commit to database
         """
-        self.bot.db.execute(f"CREATE TABLE IF NOT EXISTS {name + '_players'} "
-                            "(id INTEGER PRIMARY KEY, waitlisted INTEGER, name TEXT)")
-        self.bot.db.execute(
-            f"INSERT INTO campaigns (name, dm, role, category, information, min_players, max_players, current_players)"
-            " VALUES (?, ?, ?, ?, ?, ?, ?, 0)",
-            (name, dm_id, vals.role.id, vals.category.id, vals.information_channel, int(min_players), int(max_players)))
-        self.bot.connection.commit()
-        return True
+        try:
+            self.bot.db.execute(f"CREATE TABLE IF NOT EXISTS {self.__get_table_name(vals.name)} "
+                                "(id INTEGER PRIMARY KEY, waitlisted INTEGER, name TEXT)")
+            self.bot.db.execute(
+                f"INSERT INTO campaigns (name, dm, role, category, information_channel, min_players, max_players, current_players)"
+                " VALUES (?, ?, ?, ?, ?, ?, ?, ?)",
+                (vals.name, vals.dm, vals.role, vals.category, vals.information_channel, vals.min_players, vals.max_players, vals.current_players))
+            return True
+        except Exception as e:
+            print(e)
+            return False
 
-    def delete_campaign(self, campaign: Union[int, str]):
+    def delete_campaign(self, campaign: Union[int, str]) -> bool:
         """
         Deletes a campaign from the database
         :param campaign: Either campaign name or campaign ID
-        :return: None
+        :return: Whether we should commit to database
         """
 
-        # checks whether we passed ID or name
-        if isinstance(campaign, int):
-            resp = self.select_campaign(campaign)
-            self.bot.db.execute(f"DELETE FROM campaigns WHERE id = {campaign}")
-            self.bot.db.execute(f"DROP TABLE {resp['name'] + '_players'}")
-        else:
-            self.bot.db.execute(f"DELETE FROM campaigns WHERE name LIKE {campaign}")
-            self.bot.db.execute(f"DROP TABLE {campaign + '_players'}")
-        self.bot.connection.commit()
+        try:
+            # checks whether we passed ID or name
+            if isinstance(campaign, int):
+                resp = self.select_campaign(campaign)
+                self.bot.db.execute(f"DELETE FROM campaigns WHERE id = {campaign}")
+                self.bot.db.execute(f"DROP TABLE {self.__get_table_name(resp['name'])}")
+            else:
+                self.bot.db.execute(f"DELETE FROM campaigns WHERE name LIKE {campaign}")
+                self.bot.db.execute(f"DROP TABLE {self.__get_table_name(campaign)}")
+            return True
+        except Exception as e:
+            print(e)
+            return False
 
     def select_campaign(self, campaign: Union[int, str]) -> CampaignInfo:
         """
@@ -73,29 +78,48 @@ class CampaignSQLHelper:
         return self.bot.db.execute(f"SELECT {field} FROM campaigns").fetchall()
 
     @staticmethod
-    def dict_to_campaign(resp: dict) -> CampaignInfo:
+    def dict_to_campaign(resp: Union[sqlite3.Row, dict]) -> CampaignInfo:
         """
         Converts a row: dict returned by SELECT to a CampaignInfo object
         :param resp: The dictionary to be converted
         :return: The created CampaignInfo object
         """
+        if isinstance(resp, sqlite3.Row):
+            resp = dict(zip(resp.keys(), resp))
         campaign = CampaignInfo()
         for key, value in resp.items():
             setattr(campaign, key, value)
         return campaign
 
     def add_player(self, campaign: CampaignInfo, player: discord.Member):
-        waitlisted = 1 if campaign.current_players >= campaign.max_players else 0
-        self.bot.db.execute(f"INSERT INTO {campaign.name}_players (id, waitlisted, name) VALUES ({player.id}, {waitlisted}, {player.display_name}")
-        if not waitlisted:
-            self.__increment_players(campaign, 1)
-        self.bot.connection.commit()
+        try:
+            waitlisted = 1 if campaign.current_players >= campaign.max_players else 0
+            self.bot.db.execute(f"INSERT INTO {self.__get_table_name(campaign.name)} (id, waitlisted, name) VALUES (?, ?, ?)", (player.id, waitlisted, player.display_name))
+            if not waitlisted:
+                self.__increment_players(campaign, 1)
+            return True
+        except Exception as e:
+            print(e)
+            return False
 
     def remove_player(self, campaign: CampaignInfo, player: discord.Member):
-        self.bot.db.execute(f"DELETE FROM {campaign.name}_players WHERE id = {player.id}")
-        self.__increment_players(campaign, -1)
-        self.bot.connection.commit()
+        try:
+            self.bot.db.execute(f"DELETE FROM {self.__get_table_name(campaign.name)} WHERE id = {player.id}")
+            self.__increment_players(campaign, -1)
+            self.bot.connection.commit()
+            return True
+        except Exception as e:
+            print(e)
+            return False
 
     def __increment_players(self, campaign: CampaignInfo, amount):
-        self.bot.db.execute(f"UPDATE campaigns SET current_players = {campaign.current_players + amount} WHERE name LIKE {campaign.name}")
-        self.bot.connection.commit()
+        try:
+            self.bot.db.execute(f"UPDATE campaigns SET current_players = {campaign.current_players + amount} WHERE name LIKE ?", (campaign.name,))
+            self.bot.connection.commit()
+            return True
+        except Exception as e:
+            print(e)
+            return False
+
+    def __get_table_name(self, campaign_name: str) -> str:
+        return campaign_name.replace(" ", "_") + "_players"
