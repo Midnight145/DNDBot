faa7a75ff2892916f6fb2c4ce736dfdff03e0ffc
Ryan O'Dell <rubikscubeboy27@gmail.com>
2022-04-19 18:03:53 -0500

    commit to db when no error, add register_campaign

--- modules/CampaignManager.py
+++ modules/CampaignManager.py
@@ -2,6 +2,7 @@ import discord
 from discord.ext import commands
 import datetime
 from typing import Union, TYPE_CHECKING
+from .CampaignInfo import CampaignInfo
 
 if TYPE_CHECKING:  # TYPE_CHECKING is always false, allows for type hinting without circular import
     from ..bot import DNDBot
@@ -12,6 +13,59 @@ class CampaignManager(commands.Cog):
         self.bot = bot
         self.CampaignBuilder = self.bot.CampaignBuilder
         self.CampaignSQLHelper = self.bot.CampaignSQLHelper
+
+    @commands.command(aliases=["register"])
+    async def register_campaign(self, context: commands.Context, name: str, role: discord.Role, category: discord.CategoryChannel, information_channel: discord.TextChannel, dm: discord.Member, min_players: str, max_players: str, current_players: str):
+        """
+        :param context: Command context
+        :param name: Campaign name
+        :param role: Campaign role
+        :param category: Campaign category
+        :param information_channel: Campaign "global" information channel
+        :param dm: Campaign DM
+        :param min_players: Minimum number of players necessary to start
+        :param max_players: Maximum number of players allowed
+        :param current_players: Current number of players in the campaign
+        :return: None
+        """
+        campaign_info = CampaignInfo()
+        campaign_info.name = name
+        campaign_info.role = role.id
+        campaign_info.category = category.id
+        campaign_info.information_channel = information_channel.id
+        campaign_info.dm = dm.id
+        campaign_info.min_players = int(min_players)
+        campaign_info.max_players = int(max_players)
+        campaign_info.current_players = int(current_players)
+
+        commit = self.CampaignSQLHelper.create_campaign(campaign_info, campaign_info.current_players)
+        if commit:
+            embed = discord.Embed(
+                title="Campaign Registered",
+                description=f"Campaign {name} Registered.",
+                timestamp=datetime.datetime.utcnow()
+            )
+            embed.add_field(name="DM", value=f"{str(dm)} ({dm.id})")
+            embed.add_field(name="Role", value=str(context.guild.get_role(campaign_info["role"])))
+            embed.add_field(name="Category", value=str(context.guild.get_channel(campaign_info["category"]).name))
+            embed.add_field(name="Max Players", value=f"{max_players}")
+
+            await context.send(embed=embed)
+            self.bot.connection.commit()
+        else:
+            await context.send("Something went wrong.")
+
+    @commands.command()
+    async def delete_category(self, context: commands.Context, category: discord.CategoryChannel):
+        """
+        :param context: Command context
+        :param category: Category to delete
+        :return: None
+        """
+        for i in category.channels:
+            await i.delete()
+        await category.delete()
+        await context.send(f"Category {category.name} deleted.")
 
     @commands.command()
     @commands.has_any_role("Officer", "Dungeon Master")
@@ -27,20 +81,28 @@ class CampaignManager(commands.Cog):
         """
 
         campaign_info = await self.CampaignBuilder.create_campaign(context, name)
-        self.CampaignSQLHelper.create_campaign(name, dungeon_master.id, campaign_info, int(min_players),
-                                               int(max_players))
+        campaign_info.name = name
+        campaign_info.dm = dungeon_master.id
+        campaign_info.min_players = int(min_players)
+        campaign_info.max_players = int(max_players)
+        campaign_info.current_players = 0
 
-        embed = discord.Embed(
-            title="Campaign Created",
-            description=f"Campaign {name} created.",
-            timestamp=datetime.datetime.utcnow()
-        )
-        embed.add_field(name="DM", value=f"{str(dungeon_master)} ({dungeon_master.id})")
-        embed.add_field(name="Role", value=str(campaign_info["role"]))
-        embed.add_field(name="Category", value=str(campaign_info["category"].name))
-        embed.add_field(name="Max Players", value=f"{max_players}")
-        await context.send(embed=embed)
-        self.bot.connection.commit()
+        commit = self.CampaignSQLHelper.create_campaign(campaign_info)
+        if commit:
+            embed = discord.Embed(
+                title="Campaign Created",
+                description=f"Campaign {name} created.",
+                timestamp=datetime.datetime.utcnow()
+            )
+            embed.add_field(name="DM", value=f"{str(dungeon_master)} ({dungeon_master.id})")
+            embed.add_field(name="Role", value=str(context.guild.get_role(campaign_info["role"])))
+            embed.add_field(name="Category", value=str(context.guild.get_channel(campaign_info["category"]).name))
+            embed.add_field(name="Max Players", value=f"{max_players}")
+            await context.send(embed=embed)
+
+            self.bot.connection.commit()
+        else:
+            await context.send("Something went wrong.")
 
     @commands.command()
     @commands.has_any_role("Officer", "Dungeon Master")
@@ -52,13 +114,13 @@ class CampaignManager(commands.Cog):
         """
 
         resp = self.CampaignSQLHelper.select_campaign(campaign)
-        commit = await self.CampaignBuilder.delete_campaign(resp)
-
-        if commit:
-            await context.send(f"Campaign \"{resp['name']}\" deleted.")
+        commit = self.CampaignSQLHelper.delete_campaign(campaign)
+        commit2 = await self.CampaignBuilder.delete_campaign(resp)
+        if commit and commit2:
+            await context.send(f"Campaign \"{resp.name}\" deleted.")
             self.bot.connection.commit()
         else:
-            await context.send(f"There was an error deleting \"{resp['name']}\"")
+            await context.send(f"There was an error deleting \"{resp.name}\"")
 
     @commands.command()
     @commands.has_any_role("Officer", "Dungeon Master")
@@ -70,9 +132,9 @@ class CampaignManager(commands.Cog):
         :param campaign: Either campaign name or campaign ID
         :return: None
         """
-        self.CampaignSQLHelper.delete_campaign(campaign)
-        await context.send(f"Campaign {campaign} deregistered.")
-        self.bot.connection.commit()
+        if self.CampaignSQLHelper.delete_campaign(campaign):
+            await context.send(f"Campaign {campaign} deregistered.")
+            self.bot.connection.commit()
 
     @commands.command()
     async def list_campaigns(self, context: commands.Context):
