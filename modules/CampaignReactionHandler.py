2c56ee42b713b637786370b10ff72fc71146b4ff
Ryan O'Dell <23142346+Midnight145@users.noreply.github.com>
2022-08-29 14:51:10 -0500

    error handling, including addition of API calls and DM permission handling

--- modules/CampaignReactionHandler.py
+++ modules/CampaignReactionHandler.py
@@ -6,6 +6,7 @@ import discord
 from discord.ext import commands
 
 #from modules import Strings
+import bot
 from .CampaignBuilder import verification_denied
 
 if TYPE_CHECKING:  # TYPE_CHECKING is always false, allows for type hinting without circular import
@@ -23,6 +24,10 @@ class CampaignReactionHandler(commands.Cog):
         :return: None
         """
         channel: discord.TextChannel = self.bot.get_channel(payload.channel_id)  # queuing bot for text channel
+
+        if isinstance(channel, discord.VoiceChannel):
+            return
+
         message: discord.Message = await channel.fetch_message(payload.message_id)  # fetching message from text channel
 
         if channel.id == self.bot.config["verification_channel"]:
@@ -37,7 +42,7 @@ class CampaignReactionHandler(commands.Cog):
                 return
             embed = message.embeds[0]
             discord_id = int(embed.fields[4].value)
-            member = message.guild.get_member(discord_id)
+            member = await message.guild.fetch_member(discord_id)
             if message.author.id != self.bot.user.id:  # if message author is not the bot
                 return
             if payload.emoji.name == "✅":
@@ -63,8 +68,10 @@ class CampaignReactionHandler(commands.Cog):
             color=discord.Color.dark_red(),
             timestamp=datetime.datetime.utcnow()
         )
-        await member.send(embed=embed)
-
+        try:
+            await member.send(embed=embed)
+        except discord.Forbidden:
+            await self.bot.get_channel(self.bot.config["verification_channel"]).send(f"{member.mention}: I was unable to DM you. Please open your DMs to this server, as I will send campaign updates via DM in the future. Once you have done that, you will need to re-react to the verification reaction.")
         return False
 
     async def approve_player(self, message: discord.Message, member: discord.Member, reactor: discord.Member):
@@ -76,18 +83,17 @@ class CampaignReactionHandler(commands.Cog):
         """
         message_embed = message.embeds[0]
         campaign_name = message_embed.fields[0].value
-        dm = message_embed.fields[1].value
+        campaign = self.bot.CampaignSQLHelper.select_campaign(campaign_name)
+        dm = await message.guild.fetch_member(campaign.dm)
         player_name = message_embed.fields[2].value
         discord_tag = message_embed.fields[3].value
         player_discord = message_embed.fields[4].value
 
-        player = message.guild.get_member(int(player_discord))
-        dm = message.guild.get_member_named(dm)
+        player = await message.guild.fetch_member(int(player_discord))
 
         if reactor.id != dm.id:
             return
 
-        campaign = self.bot.CampaignSQLHelper.select_campaign(campaign_name)
         if campaign.current_players >= campaign.max_players:
             commit = self.bot.CampaignSQLHelper.waitlist_player(campaign, member)
             if commit:
@@ -114,12 +120,12 @@ class CampaignReactionHandler(commands.Cog):
         """
         message_embed = message.embeds[0]
         campaign_name = message_embed.fields[0].value
-        dm = message_embed.fields[1].value
+        campaign = self.bot.CampaignSQLHelper.select_campaign(campaign_name)
+        dm = await message.guild.fetch_member(campaign.dm)
         player_name = message_embed.fields[2].value
         player_discord = int(message_embed.fields[4].value)
 
-        player = message.guild.get_member(player_discord)
-        dm = message.guild.get_member_named(dm)
+        player = await message.guild.fetch_member(int(player_discord))
 
         if reactor.id != dm.id:
             return
