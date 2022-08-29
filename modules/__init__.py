8d7b790fe42bcdab90229628d05cb2058ce8f642
Ryan O'Dell <23142346+Midnight145@users.noreply.github.com>
2022-08-29 14:49:25 -0500

    FakeMember declaration in __init__

--- modules/__init__.py
+++ modules/__init__.py
@@ -3,4 +3,5 @@ from .CampaignInfo import CampaignInfo
 from .CampaignBuilder import CampaignBuilder
 from .CampaignManager import CampaignManager
 from .CampaignPlayerManager import CampaignPlayerManager
+from .FakeMember import FakeMember
 # from ..bot import DNDBot
