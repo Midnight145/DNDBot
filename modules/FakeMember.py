292223df62bab0248048cba0336c031d82a3ab22
Ryan O'Dell <23142346+Midnight145@users.noreply.github.com>
2022-08-29 14:47:28 -0500

    dummy class to "spoof" a discord.Member for use in remove_player

--- modules/FakeMember.py
+++ modules/FakeMember.py
@@ -1,3 +1,11 @@
 class FakeMember:
     def __init__(self, id):
         self.id = id
+        self.roles = []
+        self.mention = str(id) + " FakeMember"
+
+    async def remove_roles(self, role):
+        return
+
+    async def add_roles(self, role):
+        return
